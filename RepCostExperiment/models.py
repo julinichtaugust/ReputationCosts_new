f85c10e51b5b01f7d7f8b8b1e01d0b36ccf1b125
Julian <julinichtaugust@web.de>
2020-03-19 11:29:28 +0100

    Änderung Anzeige Marktpreis

--- RepCostExperiment/models.py
+++ RepCostExperiment/models.py
@@ -22,7 +22,7 @@ Your app description 4
 class Constants(BaseConstants):
     name_in_url = 'RepCostExperiment'
     players_per_group = 3
-    num_rounds = 1
+    num_rounds = 2
 
     endowment = 100
 
